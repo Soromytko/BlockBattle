31c0f13be72e98c33b5d4ccb45c829f5bc940c25
Soromytko <NikitaSoromytko@gmail.com>
2023-09-14 19:33:03 +0300

    add the mosaic to the game

--- Game01/Scripts/Block.gd
+++ Game01/Scripts/Block.gd
@@ -1,6 +1,10 @@
-class_name Block extends CharacterBody2D
+class_name Block extends StaticBody2D
 
 @export var health : float = 1
+
+func set_color(color : Color):
+	$Polygon2D.color = color
+	
 
 func take_damage(value : float = 1):
 	health -= value
--- Game01/Scripts/BlockGenerator.gd
+++ Game01/Scripts/BlockGenerator.gd
@@ -1,10 +1,20 @@
-class_name BlockGenerator extends Node3D
+class_name BlockGenerator extends Node2D
 
 @export var block_field_size : Vector2i
 @export var block_size : Vector2
 @export var block_scene : PackedScene
 
 func _ready():
+	var mosaic_units = MosaicFileManager.load_from_file()
+	for mosaic_unit in mosaic_units:
+		var block : Block = block_scene.instantiate()
+		block.global_position = mosaic_unit.position
+		block.set_color(mosaic_unit.color)
+		add_child(block)
+#		call_deferred("add_child", block)
+		
+	
+	return
 	var blocks = {}
 	for i in 300:
 		var x : float = randi_range(-block_field_size.x, block_field_size.x)
